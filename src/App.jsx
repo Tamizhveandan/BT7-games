5ee4913503309ff01d82b6a76f10f46393a4533d
tamizhveandan <tamizhveandan@gmail.com>
2023-04-13 02:14:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Particle from './components/particle';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Particle/>
+    </div>
+  );
+}
+
+export default App;
